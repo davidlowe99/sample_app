b096ff5c78571e26ac65df24fc4cb6a6656d2fb6
davidlowe99 <dave.lowe@outlook.com>
2013-11-15 08:48:24 -0600

    Model users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,14 +17,26 @@ describe User do
   
   it { should be_valid }
   
+  # name
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
   
+  # email
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
+  end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
    
   describe "when email format is invalid" do
@@ -58,6 +70,7 @@ describe User do
     it { should_not be_valid }
   end
   
+  # password
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
@@ -66,13 +79,12 @@ describe User do
     it { should_not be_valid }
   end
   
-  
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
   
-  #Authentication tests
+  # Authentication tests
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
